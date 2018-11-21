6d810c4bf2b85aa8175c6a08d08fe5d858d2b6e1
Tyler Schrock <tschrock123@gmail.com>
2018-11-21 10:51:08 -0500

    Add some changes I forgot to push

---
+++ lib/index.ts
@@ -0,0 +1,18 @@
+/*!
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+'use strict';
+
+export * from './client/BS4Templates';
+export * from './client/ClientInterfaces';
+export * from './client/FA4Icons';
+export * from './client/FA5Icons';
+export * from './Condition';
+export * from './Converters';
+export * from './Filter';
+export * from './Operator';
+export * from './OperatorOverload';
+export * from './QueryBuilder';
+export * from './Type';
